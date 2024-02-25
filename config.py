c23f168e2ff7332cdb931dd47da5d8b9ceefd85a
Sergey Okazin <kyriha@mail.ru>
2024-02-25 12:20:56 +1000

    Fist

---
+++ config.py
@@ -0,0 +1,8 @@
+# Зададим имя базы данных
+DB_NAME = 'quiz_bot.db'
+
+# Переменная с адресом файла с вопросами
+DICT_DATA = 'data\quiz_data.json'
+
+# Замените "YOUR_BOT_TOKEN" на токен, который вы получили от BotFather
+API_TOKEN = '6846668850:AAGKq15Fga-cxPwVgsMP8IyohNTFN1EMs40'
